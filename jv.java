663834946aea8fbf9beb8b0385d56143afc7e458
gilberthon <gilbert.95.hon@gmail.com>
2018-11-05 20:23:38 +0800

    ad

--- jv.java
+++
@@ -1,8 +0,0 @@
-class J{
-    public static void main(String[] args){
-        System.out.println("java");
-        for(int i = 0; i< 10; i++){
-            System.out.println(i + 1);
-        }
-    }
-}
